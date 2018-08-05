a0eb4323f3a81498ad0be7890b7874442b1d6a46
user1 <bismark.john.anes@gmail.com>
2018-08-06 00:16:31 +0700

    Selector using variable

--- js/index.js
+++ js/index.js
@@ -6,8 +6,27 @@ $(document).ready(function() {
   var o_win = 0;
   var x_win = 0;
   $('#game li').click(function(){
+    var oneClassO = $("#one").hasClass('o');
+    var twoClassO = $("#two").hasClass('o');
+    var threeClassO = $("#three").hasClass('o');
+    var fourClassO = $("#four").hasClass('o');
+    var fiveClassO = $("#five").hasClass('o');
+    var sixClassO = $("#six").hasClass('o');
+    var sevenClassO = $("#seven").hasClass('o');
+    var eightClassO = $("#eight").hasClass('o');
+    var nineClassO = $("#nine").hasClass('o');
 
-    if ($("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') || $("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') || $("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') || $("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') || $("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') || $("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o'))
+    var oneClassX = $("#one").hasClass('x');
+    var twoClassX = $("#two").hasClass('x');
+    var threeClassX = $("#three").hasClass('x');
+    var fourClassX = $("#four").hasClass('x');
+    var fiveClassX = $("#five").hasClass('x');
+    var sixClassX = $("#six").hasClass('x');
+    var sevenClassX = $("#seven").hasClass('x');
+    var eightClassX = $("#eight").hasClass('x');
+    var nineClassX = $("#nine").hasClass('x');
+
+    if (oneClassO && twoClassO && threeClassO || fourClassO && fiveClassO && sixClassO || sevenClassO && eightClassO && nineClassO || oneClassO && fourClassO && sevenClassO || twoClassO && fiveClassO && eightClassO || threeClassO && sixClassO && nineClassO || oneClassO && fiveClassO && nineClassO || threeClassO && fiveClassO && sevenClassO)
     {
       alert('O has won the game. Start a new game');
       $("#game li").text("+");
@@ -17,7 +36,7 @@ $(document).ready(function() {
       $("#game li").removeClass('btn-primary');
       $("#game li").removeClass('btn-info');
     }
-    else if ($("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') || $("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') || $("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') || $("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') || $("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') || $("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x'))
+    else if (oneClassX && twoClassX && threeClassX || fourClassX && fiveClassX && sixClassX || sevenClassX && eightClassX && nineClassX || oneClassX && fourClassX && sevenClassX || twoClassX && fiveClassX && eightClassX || threeClassX && sixClassX && nineClassX || oneClassX && fiveClassX && nineClassX || threeClassX && fiveClassX && sevenClassX)
     {
       alert('X wins has won the game. Start a new game');
       $("#game li").text("+");
@@ -47,7 +66,18 @@ $(document).ready(function() {
       count++;
       $(this).text(o);
       $(this).addClass('disable o btn-primary');
-      if ($("#one").hasClass('o') && $("#two").hasClass('o') && $("#three").hasClass('o') || $("#four").hasClass('o') && $("#five").hasClass('o') && $("#six").hasClass('o') || $("#seven").hasClass('o') && $("#eight").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#four").hasClass('o') && $("#seven").hasClass('o') || $("#two").hasClass('o') && $("#five").hasClass('o') && $("#eight").hasClass('o') || $("#three").hasClass('o') && $("#six").hasClass('o') && $("#nine").hasClass('o') || $("#one").hasClass('o') && $("#five").hasClass('o') && $("#nine").hasClass('o') || $("#three").hasClass('o') && $("#five").hasClass('o') && $("#seven").hasClass('o'))
+
+      oneClassO = $("#one").hasClass('o');
+      twoClassO = $("#two").hasClass('o');
+      threeClassO = $("#three").hasClass('o');
+      fourClassO = $("#four").hasClass('o');
+      fiveClassO = $("#five").hasClass('o');
+      sixClassO = $("#six").hasClass('o');
+      sevenClassO = $("#seven").hasClass('o');
+      eightClassO = $("#eight").hasClass('o');
+      nineClassO = $("#nine").hasClass('o');
+
+      if (oneClassO && twoClassO && threeClassO || fourClassO && fiveClassO && sixClassO || sevenClassO && eightClassO && nineClassO || oneClassO && fourClassO && sevenClassO || twoClassO && fiveClassO && eightClassO || threeClassO && sixClassO && nineClassO || oneClassO && fiveClassO && nineClassO || threeClassO && fiveClassO && sevenClassO)
       {
         alert('O wins');
         count = 0;
@@ -60,7 +90,18 @@ $(document).ready(function() {
       count++;
       $(this).text(x);
       $(this).addClass('disable x btn-info');
-      if ($("#one").hasClass('x') && $("#two").hasClass('x') && $("#three").hasClass('x') || $("#four").hasClass('x') && $("#five").hasClass('x') && $("#six").hasClass('x') || $("#seven").hasClass('x') && $("#eight").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#four").hasClass('x') && $("#seven").hasClass('x') || $("#two").hasClass('x') && $("#five").hasClass('x') && $("#eight").hasClass('x') || $("#three").hasClass('x') && $("#six").hasClass('x') && $("#nine").hasClass('x') || $("#one").hasClass('x') && $("#five").hasClass('x') && $("#nine").hasClass('x') || $("#three").hasClass('x') && $("#five").hasClass('x') && $("#seven").hasClass('x'))
+
+      oneClassX = $("#one").hasClass('x');
+      twoClassX = $("#two").hasClass('x');
+      threeClassX = $("#three").hasClass('x');
+      fourClassX = $("#four").hasClass('x');
+      fiveClassX = $("#five").hasClass('x');
+      sixClassX = $("#six").hasClass('x');
+      sevenClassX = $("#seven").hasClass('x');
+      eightClassX = $("#eight").hasClass('x');
+      nineClassX = $("#nine").hasClass('x');
+
+      if (oneClassX && twoClassX && threeClassX || fourClassX && fiveClassX && sixClassX || sevenClassX && eightClassX && nineClassX || oneClassX && fourClassX && sevenClassX || twoClassX && fiveClassX && eightClassX || threeClassX && sixClassX && nineClassX || oneClassX && fiveClassX && nineClassX || threeClassX && fiveClassX && sevenClassX)
       {
         alert('X wins');
         count = 0;
